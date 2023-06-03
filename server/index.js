9152bf46dff24121a1322b3dd56fb3e1d9af66e6
AngelinaPierre <gaptp95@gmail.com>
2023-06-03 17:54:51 -0300

    client4

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // entry point for backend
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -11,15 +12,9 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const port = process.env.PORT || config.PORT;
-const uri = config.MONGODB_URI;
-
-// connecting to mongodb
-mongoose.connect(uri, {
-    useNewUrlParser: true
-}).then(() => console.log('MONGDB connected!')).catch(err => console.error(err))
-
-// creating server
+const uri = process.env.MONGODB_URI || config.MONGODB_URI;
 const app = express();
+// creating server
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,14 +22,27 @@ app.use(cookieParser());
 
 app.use('/api/users', require('./routes/user'))
 
+const server = http.createServer(app);
+// connecting to mongodb
+mongoose.connect(uri, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('MONGDB connected!');
+    server.listen(port, port, () => {
+        console.log(`Backend is running on port ${port}`)
+    })
+}).catch(err => console.error(err))
+
+
 // // route and link
 // app.get('/', (req, res) => {
 //     res.json({ "Hello": "I am happy to deploy our application" })
 // })
 
 
-app.listen(port, () => {
-    console.log(`Backend is running on port ${port}`)
-});
+
+// app.listen(port, () => {
+//     console.log(`Backend is running on port ${port}`)
+// });
 
 module.exports = app
