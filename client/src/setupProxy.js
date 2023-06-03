b91a61356c862b4fcb8b0a4ec7f2aaf8fcccdb39
AngelinaPierre <gaptp95@gmail.com>
2023-06-03 17:39:37 -0300

    client2

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
         '/api',
         createProxyMiddleware({
             // target: 'http://localhost:5000',
-            target: 'https://online-shop-mern-boilerplate.vercel.app/',
+            target: 'https://online-shop-mern-boilerplate.vercel.app/api/',
             changeOrigin: true,
         })
     );
